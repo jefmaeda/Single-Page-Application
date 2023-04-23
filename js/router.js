9eeb7105b46da1b7ffb4a3e2c8ed6eb6647250b1
Jef <jefersontamono@gmail.com>
2023-04-24 01:26:25 +0900

    update, page is moter

--- js/router.js
+++ js/router.js
@@ -1,4 +1,6 @@
-class Router{
+export class Router{
+    routes = {}
+
     add(routeName, page){
         this.routes[routeName] = page
     }
@@ -23,3 +25,4 @@ class Router{
         })
     }
 }
+
